b1ea337039365440a070eef924995f531f44d5e5
bemuse01 <sequeland@gmail.com>
2023-03-16 20:25:52 +0900

    update

---
+++ components/PadsBox.vue
@@ -0,0 +1,51 @@
+<template>
+    <div 
+        :class="boxClass"
+    >
+
+        <div
+            :class="padClass"
+            v-for="pad in pads"
+            :key="pad.key"
+        >
+
+            <div
+                :class="padCellClass"
+                v-for="cell in pad.cells"
+                :key="cell.key"
+            >
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import {toRefs, computed} from 'vue'
+
+
+// props
+const props = defineProps({
+    count: Number,
+    beats: Number,
+})
+
+
+// variable
+const {count, beats} = toRefs(props)
+const pads = computed(() => Array.from({length: count.value}, (_, key) => ({
+    key,
+    cells: Array.from({length: beats.value}, (_, key2) => ({key: key2}))
+})))
+
+
+// class
+const boxClass = 'pads-box pr-1 flex-1 flex flex-col overflow-auto gap-1 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-transparent'
+const padClass = 'flex gap-1'
+const padCellClass = 'flex-1 aspect-square bg-slate-700 rounded-lg'
+</script>
+
+<style>
+
+</style>
--- pages/pad.vue
+++ pages/pad.vue
@@ -1,14 +1,39 @@
 <template>
-    
+    <div 
+        :class="containerClass"
+    >
+
+        <pads-box :count="count" :beats="beats" />
+
+        <div
+            :class="controlBoxClass"
+        >
+        </div>
+
+    </div>
 </template>
 
 <script setup>
+import {} from 'vue'
+
+
+// variable
+const instruments = ['rock', 'jazz', 'hiphop', 'percussion']
+const count = 12
+const beats = ref(4 * 4)
+
+
+// class
+const containerClass = 'pad-container w-[80%] h-[100vh] py-4 mx-auto flex flex-col gap-2'
+const controlBoxClass = 'control-box w-full h-[4rem]'
+
+
 // head
 useHead({
     title: 'Pad - Drum Pad',
 })
 </script>
 
-<style>
+<style scoped>
 
 </style>
