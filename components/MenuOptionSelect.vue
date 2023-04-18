57bbffd36047557f407dcbcffaf531bc011e9bc6
bemuse01 <sequeland@gmail.com>
2023-04-18 17:39:24 +0900

    update

---
+++ components/MenuOptionSelect.vue
@@ -0,0 +1,61 @@
+<template>
+    <div
+        :class="optionClass"
+    >
+        <span>{{name}}</span>
+
+        <select @change="e => updateValue(e.target.value)">
+
+            <option
+                v-for="item in list"
+                :key="item.key"
+            >
+                {{item.name}}
+            </option>
+
+        </select>
+
+    </div>
+</template>
+
+<script setup>
+import {toRefs} from 'vue'
+
+
+// props
+const props = defineProps({
+    name: {
+        type: String,
+        default: ''
+    },
+    list: {
+        type: Array,
+        default: []
+    },
+    selected: {
+        type: String,
+    }
+})
+
+
+// emit
+const emit = defineEmits(['update:selected'])
+
+
+// variable
+const {name, list} = toRefs(props)
+
+
+// class
+const optionClass = 'flex flex-row justify-between my-8 text-lime-500 text-2xl'
+
+
+// method
+const updateValue = (value) => {
+    emit('update:selected', value)
+}
+</script>
+
+<style scoped>
+    
+</style>
--- components/MenuSide.vue
+++ components/MenuSide.vue
@@ -3,12 +3,15 @@
         :class="sideClass"
     >
         
+        <!-- <menu-option name="Instrument"></menu-option>
+        <menu-option name="Beat"></menu-option> -->
+
     </div>
 </template>
 
 <script setup>
 // class
-const sideClass = 'menu-side absolute top-0 right-0 w-[24rem] h-full'
+const sideClass = 'menu-side absolute top-0 right-0 w-[24rem] h-full p-4 overflow-auto'
 </script>
 
 <style>
